ca024d97f9820dfa77dac2dfcebbbcb1b798cbee
acsuper <168913086+anshroyal@users.noreply.github.com>
2025-06-25 23:14:32 +0530

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,5 +4,4 @@ echo "Downloading u2net.onnx model..."
 wget -O u2net.onnx https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx
 echo "Model downloaded."
 
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
+
