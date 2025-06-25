f2cf9072699d14f6998bbe641074478e8cacd7b3
acsuper <168913086+anshroyal@users.noreply.github.com>
2025-06-25 21:08:40 +0530

    process.py

--- process.py
+++ process.py
@@ -5,5 +5,5 @@ import io
 def remove_img(image_bytes):
     out_bytes = remove(image_bytes)
     out_img = Image.open(io.BytesIO(out_bytes))
-    out_img.show()  # Optional: remove if running on server without display
+    #out_img.show()  # Optional: remove if running on server without display
     return out_bytes
