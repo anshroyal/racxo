a87454257c987969faad5d46e7f5c5817886541a
acsuper <168913086+anshroyal@users.noreply.github.com>
2025-06-25 22:37:29 +0530

    process.py

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
-from rembg import remove
+from rembg import remove, new_session
 from PIL import Image
 import io
 
+# Create a session with your local model
+session = new_session(model_name="u2net", model_path="u2net.onnx")
+
 def remove_img(image_bytes):
-    out_bytes = remove(image_bytes)
+    out_bytes = remove(image_bytes, session=session)
     out_img = Image.open(io.BytesIO(out_bytes))
-    #out_img.show()  # Optional: remove if running on server without display
     return out_bytes
