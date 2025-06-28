53e5f7d131d4b97fd8adad1064fec8dc32a4693b
acsuper <168913086+anshroyal@users.noreply.github.com>
2025-06-28 16:14:22 +0530

    process.py

--- process.py
+++ process.py
@@ -1,21 +1,8 @@
 
 from PIL import Image
+from rembg import remove 
 import io
 
 def remove_img(image_bytes):
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGBA")
-
-    datas = image.getdata()
-    new_data = []
-    for item in datas:
-        # Simple rule: make pixels white-ish transparent
-        if item[0] > 200 and item[1] > 200 and item[2] > 200:
-            new_data.append((255, 255, 255, 0))
-        else:
-            new_data.append(item)
-
-    image.putdata(new_data)
-    
-    output = io.BytesIO()
-    image.save(output, format='PNG')
-    return output.getvalue()
+    out_bytes = remove(image_bytes)
+    return out_bytes
